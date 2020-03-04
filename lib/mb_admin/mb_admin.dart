058fe15ea6267699bb3aa5f47d9c1cb08bc4fb15
lorenzOliveto <lorenzo.oliveto@gmail.com>
2020-03-04 15:32:53 +0100

    Added section edit

--- lib/mb_admin/mb_admin.dart
+++ lib/mb_admin/mb_admin.dart
@@ -42,4 +42,35 @@ class MBAdmin {
 
     MBManager.checkResponse(responseString);
   }
+
+  Future<void> editSection(
+      int sectionId, List<MBUploadableElement> elements) async {
+    String apiName = 'api/sections/' + sectionId.toString() + '/update';
+
+    var uri = Uri.https(MBManager.shared.endpoint, apiName);
+    var request = http.MultipartRequest('POST', uri);
+    for (MBUploadableElement element in elements) {
+      List<MBMultipartForm> forms = element.toForm();
+      if (forms != null) {
+        for (MBMultipartForm form in forms) {
+          if (!form.isFile) {
+            request.fields[form.name] = form.value;
+          } else {
+            request.files.add(
+              await http.MultipartFile.fromPath(
+                form.name,
+                form.path,
+                contentType: form.mimeType,
+              ),
+            );
+          }
+        }
+      }
+    }
+    request.headers.addAll(await MBManager.shared.headers());
+    http.StreamedResponse response = await request.send();
+    final responseString = await response.stream.bytesToString();
+
+    MBManager.checkResponse(responseString);
+  }
 }
