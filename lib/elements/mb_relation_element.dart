ba43b8d9c454b870042d05430a53e0431a361134
lorenzOliveto <lorenzo.oliveto@gmail.com>
2020-03-05 11:04:43 +0100

    Commit

--- lib/elements/mb_relation_element.dart
+++ lib/elements/mb_relation_element.dart
@@ -4,10 +4,16 @@ class MBRelationElement extends MBElement {
   int blockId;
   int sectionId;
 
-  MBRelationElement({Map<String, dynamic> dictionary}) : super(dictionary: dictionary) {
-    if (dictionary['block_id'] != null && dictionary['section_id'] != null) {
-      blockId = int.tryParse(dictionary['block_id']) ?? 0;
-      sectionId = int.tryParse(dictionary['section_id']) ?? 0;
+  MBRelationElement({Map<String, dynamic> dictionary})
+      : super(dictionary: dictionary) {
+    if (dictionary['value'] != null) {
+      if (dictionary['value'] is Map<String, dynamic>) {
+        Map<String, dynamic> value = dictionary['value'];
+        if (value['block_id'] != null && value['section_id'] != null) {
+          blockId = int.tryParse(value['block_id']) ?? 0;
+          sectionId = int.tryParse(value['section_id']) ?? 0;
+        }
+      }
     }
   }
 }
